11f1e1439d337b7da9569e1660f89a05f16b03b4
GD Jung <guedon.jung@gmail.com>
2016-01-02 21:24:22 +0900

    modify run-client, run-server

--- run-client.sh
+++ run-client.sh
@@ -6,5 +6,6 @@ CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
 
 IP=$(docker inspect $CONTAINER_ID | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["NetworkSettings"]["IPAddress"]')
 
-mysql -u root -p -h $IP
+# mysql -u root -p -h $IP
+mysql -u root -p -h 192.168.99.100
 
--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root mysql
+docker run -d -p 3306:3306 --name=gd452.mysql -e MYSQL_ROOT_PASSWORD=root gd452/mysql
 
